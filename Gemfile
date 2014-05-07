af277437066fc069faa745dfc9f33f40543056ab
blueyiniu <blueyiniu@qq.com>
2014-05-08 00:47:48 +0800

    Add guard and spok for BDD

--- Gemfile
+++ Gemfile
@@ -11,15 +11,18 @@ gem 'will_paginate', '3.0.4'
 gem 'bootstrap-will_paginate', '0.0.9'
 gem 'devise'
 gem 'carrierwave'
+group :development, :test, :production do
+  gem 'pg', '0.15.1'
+end
 
 group :development, :test do
-  gem 'sqlite3', '1.3.8'
+  #  gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
   # The following optional lines are part of the advanced setup.
-  # gem 'guard-rspec', '2.5.0'
-  # gem 'spork-rails', '4.0.0'
-  # gem 'guard-spork', '1.5.0'
-  # gem 'childprocess', '0.3.6'
+  gem 'guard-rspec', '2.5.0'
+  gem 'spork-rails', '4.0.0'
+  gem 'guard-spork', '1.5.0'
+  gem 'childprocess', '0.3.6'
 end
 
 group :test do
@@ -53,6 +56,5 @@ group :doc do
 end
 
 group :production do
-  gem 'pg', '0.15.1'
   gem 'rails_12factor', '0.0.2'
 end
--- spec/requests/base_pages_spec.rb
+++ spec/requests/base_pages_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe "base pages" do
+  let(:base_title)   { "Picture Share" } 
 
   describe "Home page" do
 	it "should have the content 'Picture Share'" do
 	  visit '/base_pages/home'
-	  expect(page).to have_content('Picture Share')
+	  expect(page).to have_content("#{base_title}")
+	end
+
+	it "should have the title 'Home'" do
+	  visit '/base_pages/home'
+	  expect(page).to have_title("#{base_title} | Home")
 	end
   end
 
@@ -14,6 +20,12 @@ describe "base pages" do
 	  visit '/base_pages/help'
 	  expect(page).to have_content('Help')
 	end
+
+	it "should have the title 'Help'" do
+	  visit '/base_pages/help'
+	  expect(page).to have_title("#{base_title} | Help")
+	end
+
   end
 
   describe "About" do
@@ -21,6 +33,12 @@ describe "base pages" do
 	  visit '/base_pages/about'
 	  expect(page).to have_content('About Us')
 	end
+
+	it "should have the title 'About Us'" do
+	  visit '/base_pages/about'
+	  expect(page).to have_title("#{base_title} | About Us")
+	end
+
   end
 
 end
