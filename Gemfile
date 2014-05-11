b38bf64184bbdbd480436ccfa95bdba1572304b9
blueyiniu <blueyiniu@qq.com>
2014-05-11 15:01:51 +0800

    Finish login and logout

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'https://ruby.taobao.org/'
+source 'https://rubygems.org/'
 # ruby '2.0.0-p451'
 #ruby-gemset=Pic_Share
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+
+
   def new
 	@user = User.new
   end
@@ -7,4 +12,27 @@ class UsersController < ApplicationController
 	@user = User.find(params[:id])
   end
 
+  def edit
+	@user = User.find(params[:id])
+  end
+
+  def index
+	@users = User.paginate(page: params[:page])
+  end
+
+  private
+  def user_params
+	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def signed_in_user
+	unless signed_in?
+	  redirect_to new_user_session_path, notice: "Please sign in."
+	end
+  end
+
+  def correct_user
+	@user = User.find(params[:id])
+	redirect_to(root_path) unless current_user == @user
+  end
 end
